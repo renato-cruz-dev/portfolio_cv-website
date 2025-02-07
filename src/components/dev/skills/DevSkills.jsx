4f71f2ae7caf35491115516119ae95540c38bd65
renato-cruz-dev <renatogcruz96@gmail.com>
2025-02-07 15:52:23 +0000

    Change skills component

--- src/components/dev/skills/DevSkills.jsx
+++ src/components/dev/skills/DevSkills.jsx
@@ -16,12 +16,6 @@ const DevSkills = ({ data, filter, getOrder }) => {
             className="dev-skill"
             key={skill.language}
           >
-            <div className="skill-img">
-              <img
-                src={`../../../assets/${skill.icon}`}
-                alt={`${skill.language} icon`}
-              />
-            </div>
             <p className="text__regular">{skill.language}</p>
           </div>
         );
